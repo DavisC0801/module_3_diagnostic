a9599e525f9a1ad08909763779b87e9e81211c5d
davisc0801 <davisc1014@gmail.com>
2019-07-08 09:15:53 -0600

    Add welcome page spec for search button pathing

---
+++ spec/features/welcome_page_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "welcome page" do
+  describe "as a user, when I visit /" do
+    describe "and I fill in the search form with sweet potatoes" do
+      it "should take me to the /foods path when clicking search" do
+        visit root_path
+
+        click_on "Search"
+
+        expect(current_path).to eq(foods_path)
+      end
+    end
+  end
+end
