d7f8ef4dd4b7dcebdf9b5c531a0e8efd35dc462e
davisc0801 <davisc1014@gmail.com>
2019-07-08 11:05:12 -0600

    Add food service spec for foods data method

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -4,9 +4,18 @@ describe FoodService do
   before :each do
     @test_service = FoodService.new("Sweet potatoes")
   end
+
   it "#total_items" do
-    VCR.use_cassette("/foods/food_service") do
+    VCR.use_cassette("/foods/food_service", allow_playback_repeats: true) do
       expect(@test_service.total_items).to eq(531)
+    end
+  end
+
+  it "#foods_data" do
+    VCR.use_cassette("/foods/food_service", allow_playback_repeats: true) do
+      expect(@test_service.foods_data.count).to eq(10)
+      expect(@test_service.foods_data.first[:ds]).to eq("LI")
+      expect(@test_service.foods_data.first[:ndbno]).to eq("45094945")
     end
   end
 end
