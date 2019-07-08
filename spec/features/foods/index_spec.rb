fd6d6e187cd3b82820436ad72edf479cd281df03
davisc0801 <davisc1014@gmail.com>
2019-07-08 10:19:29 -0600

    Add cassettes for index and welcome feature tests

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -4,10 +4,13 @@ describe "when on /foods" do
   before :each do
     visit root_path
     fill_in "q", with: "Sweet potatoes"
-    click_on "Search"
   end
 
   it "should show the total number of items returned by the search" do
-    expect(page).to have_content("531 items found.")
+    VCR.use_cassette("foods/index") do
+      click_on "Search"
+      
+      expect(page).to have_content("531 items found.")
+    end
   end
 end
--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -4,11 +4,14 @@ describe "welcome page" do
   describe "as a user, when I visit /" do
     describe "and I fill in the search form with sweet potatoes" do
       it "should take me to the /foods path when clicking search" do
-        visit root_path
+        VCR.use_cassette("foods/welcome") do
+            
+          visit root_path
 
-        click_on "Search"
+          click_on "Search"
 
-        expect(current_path).to eq(foods_path)
+          expect(current_path).to eq(foods_path)
+        end
       end
     end
   end
