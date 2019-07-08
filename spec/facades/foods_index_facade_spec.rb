86aea5f7ba66a0e189c967adfc02282252aa7213
davisc0801 <davisc1014@gmail.com>
2019-07-08 10:58:41 -0600

    Add facade test for foods method

--- spec/facades/foods_index_facade_spec.rb
+++ spec/facades/foods_index_facade_spec.rb
@@ -5,8 +5,15 @@ describe FoodsIndexFacade do
     @test_facade = FoodsIndexFacade.new("Sweet potatoes")
   end
   it "returns a count of food items from service" do
-    VCR.use_cassette("foods/food_index_facade") do
+    VCR.use_cassette("foods/food_index_facade", allow_playback_repeats: true) do
       expect(@test_facade.items_count).to eq(531)
+    end
+  end
+  it "returns a list of food items from service" do
+    VCR.use_cassette("foods/food_index_facade", allow_playback_repeats: true) do
+      expect(@test_facade.foods.count).to eq(10)
+      expect(@test_facade.foods_data.first.ndb_number).to eq("45094945")
+      expect(@test_facade.foods_data.last.ndb_number).to eq("11516")
     end
   end
 end
