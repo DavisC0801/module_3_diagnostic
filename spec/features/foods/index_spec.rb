8c35769430eef9d56bd51bb795209e7fc068e6ad
davisc0801 <davisc1014@gmail.com>
2019-07-08 10:36:10 -0600

    Add spec for displaying food items on food index page view

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -9,8 +9,24 @@ describe "when on /foods" do
   it "should show the total number of items returned by the search" do
     VCR.use_cassette("foods/index") do
       click_on "Search"
-      
+
       expect(page).to have_content("531 items found.")
+    end
+  end
+
+  it "should show a list of ten foods sorted by relevance." do
+    within(".food-information") do
+      expect(page).to have_content("NDB Number: 45094945")
+      expect(page).to have_content("Name: ONE POTATO TWO POTATO, PLAIN JAYNES, SWEET POTATO CHIPS, UPC: 785654000544")
+      expect(page).to have_content("Food Group: Branded Food Products Database")
+      expect(page).to have_content("Data Source: Branded Food Products")
+      expect(page).to have_content("Manufacturer: Dieffenbach's Potato Chips")
+
+      expect(page).to have_content("NDB Number: 11516")
+      expect(page).to have_content("Name: Sweet potato, frozen, unprepared (Includes foods for USDA's Food Distribution Program)")
+      expect(page).to have_content("Food Group: Vegetables and Vegetable Products")
+      expect(page).to have_content("Data Source: Standard Reference")
+      expect(page).to have_content("Manufacturer: none")
     end
   end
 end
